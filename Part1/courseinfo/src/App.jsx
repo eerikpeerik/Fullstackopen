b1d933e56d552044a5c712926327f86f207ef670
eerikpeerik <erik.astrom@abo.fi>
2026-01-19 14:34:02 +0200

    Part1.3 completed

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,49 +1,59 @@
 const App = () => {
-    const content = 
-    {
-      headerText: "Half Stack application development",
-      info: [
-    { part: 'Fundamentals of React', amount: 10},
-    { part: 'Using props to pass data', amount: 7},
-    { part: 'State of a component', amount: 14}
-      ]
+  console.log("App started")
+  const course = 'Half Stack application development'
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
     }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    }
+
+    const t = [part1, part2, part3]
   return (
     <div>
-      <Header name={content}/>
-      <Content content={content}/>
-      <Total totalExercises={content}/>
+      <Header name={course}/>
+      <Content content={t}/>
+      <Total totalExercises={t}/>
     </div>
   )
 }
 
 const Header = (props) => {
+  console.log("Header called")
   return (
-      <h1>{props.name.headerText}</h1>
+      <h1>{props.name}</h1>
   )
 }
 
 const Content = (props) => {
-
+  console.log("Content called")
+  console.log("Prop content 0 =" + props.content[0])
   return(
     <div>
-      <Part gosh={props.content.info[0]}/>
-      <Part gosh={props.content.info[1]}/>
-      <Part gosh={props.content.info[2]}/>
+      <Part gosh={props.content[0]}/>
+      <Part gosh={props.content[1]}/>
+      <Part gosh={props.content[2]}/>
     </div>
   )
 }
 
 const Total = (props) => {
+  console.log("Total called")
   return(
-    <p>Number of exercises {props.totalExercises.info[0].amount + props.totalExercises.info[1].amount + props.totalExercises.info[2].amount}</p>
+    <p>Number of exercises {props.totalExercises[0].exercises + props.totalExercises[1].exercises + props.totalExercises[2].exercises}</p>
   )
 }
 
 const Part = (props) => {
-
+  console.log("Part called")
   return (
-      <p>{props.gosh.part} {props.gosh.amount}</p>
+      <p>{props.gosh.name} {props.gosh.exercises}</p>
  
   )
 }
